6d5362e4b2a6500c2e94fc4a7887dac483fce20f
chiragsingh1 <imchirag.2717@gmail.com>
2022-04-14 18:23:48 +0530

    creating a user and saving it in db

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,42 @@
 // User Controller
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 
+// Logic to create a new user and saving it in mongoDB
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "User Registered",
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400).json({
+      error: "Please send all the fields.",
+    });
+    throw new Error("Please send all the fields.");
+  }
+
+  // If the user already exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400).json({ error: "User already exists." });
+    throw new Error("User already exists.");
+  }
+
+  // Else Create a new user
+  const user = await User.create({
+    name,
+    email,
+    password,
   });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400).json({ error: "Failed to create user." });
+    throw new Error("Failed to create user.");
+  }
 });
 
 module.exports = { registerUser };
